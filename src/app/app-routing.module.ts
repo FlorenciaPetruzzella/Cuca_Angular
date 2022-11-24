3156f4e028a4ea3e71d106e60562d5f4c4af6fdd
Florencia Petruzzella <florenciapetruzzella1@gmail.com>
2022-11-24 16:23:16 -0300

    Se agregó componente cart y about, y se armo el router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAboutComponent } from './product-about/product-about.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'products'},
+  {path:'products', component: ProductListComponent},
+  {path:'abaout', component: ProductAboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { CaptionComponent } from './caption/caption.component';
 
 import { FormsModule } from '@angular/forms';
+import { CartComponent } from './cart/cart.component';
+import { ProductAboutComponent } from './product-about/product-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    CaptionComponent
+    CartComponent,
+    ProductAboutComponent
   ],
   imports: [
     BrowserModule,
